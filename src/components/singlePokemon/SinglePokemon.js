0039b229ffa5f2f28303b28f7f19ff96eac5cc7b
Corey Lutrick <coreylutrick@gmail.com>
2018-07-24 18:16:06 -0700

    lots of data change and fixed bug

--- src/components/singlePokemon/SinglePokemon.js
+++ src/components/singlePokemon/SinglePokemon.js
@@ -46,8 +46,29 @@ class SinglePokemon extends React.Component
     const mySinglePokemon = this.state.pokemon;
     return (
       <div>
-        <img src={mySinglePokemon.imgUrl} alt={mySinglePokemon.name}/>
+        <h1 className="text-center">{mySinglePokemon.name}</h1>
+        <div className="row singlePokemonContainer">
+          <div className="col-xs-2 col-xs-offset-2">
+            <h4>Base Stats:</h4>
+            <ul>
+              <li><strong>Hp:</strong> {mySinglePokemon.hp}</li>
+              <li><strong>Attack:</strong> {mySinglePokemon.attack}</li>
+              <li><strong>Defense:</strong> {mySinglePokemon.defense}</li>
+              <li><strong>Sp. Attack:</strong> {mySinglePokemon.spAtk}</li>
+              <li><strong>Sp. Defense:</strong> {mySinglePokemon.spDef}</li>
+              <li><strong>Speed:</strong>{mySinglePokemon.spd}</li>
+            </ul>
+            <h3>Total: {mySinglePokemon.total}</h3>
+          </div>
+          <div>
+            <img className="col-xs-4 pokeImg" src={mySinglePokemon.imgUrl} alt={mySinglePokemon.name}/>
+          </div>
+        </div>
+        <div className="col-xs-4 col-xs-offset-4 text-center">
+          <h4>{mySinglePokemon.description}</h4>
+        </div>
         <button onClick={this.deletePokemonClick}>X</button>
+        <button>Nickname</button>
       </div>
     );
   }
