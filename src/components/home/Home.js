5c2db27b84acdcd830b0285343049a63d0949f01
Corey Lutrick <coreylutrick@gmail.com>
2018-08-02 15:19:34 -0700

    added different color to body, created a text box style for pokemon themed boxes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,6 +9,16 @@ class Home extends React.Component
     return (
       <div>
         <h2 className="text-center">Home</h2>
+        <div className="row">
+          <div className="text-center">
+            <img alt="professor oak" src="https://vignette.wikia.nocookie.net/p__/images/8/84/Professor_Oak_XY.png/revision/latest?cb=20131206152342&path-prefix=protagonist"/>
+          </div>
+          <div className="row text-center">
+            <div className="text-center box col-xs-4 col-xs-offset-4">
+              <p>Welcome to the world of Pokemon!</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
